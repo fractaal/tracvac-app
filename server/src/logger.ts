a4c31dda459aa15b67ae5a050f36adcc4b246018
fractaal <sonoroustwo@gmail.com>
2021-03-08 23:04:22 +0800

    🔧 change formatting

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,11 +1,11 @@
 import chalk from 'chalk';
 
 export default function register (name: string) {
-  console.log(`[INFO Logger] Registered new subsystem ${name}.`);
+  console.log(chalk.blue(`[NEW:Logger] Registered new subsystem ${name}.`));
   return {
-    log: (...data: any[]) => console.log(`[INFO ${name}]`, ...data),
-    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[WARN ${name}]`, ...data)),
-    error: (...data: any[]) => console.error(chalk.bgRedBright(`[ERR ${name}]`, ...data)),
-    success: (...data: any[]) => console.log(chalk.greenBright(`[SUCCESS ${name}]`,...data)),
+    log: (...data: any[]) => console.log(`[INF:${name.toUpperCase()}]`, ...data),
+    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[WRN:${name.toUpperCase()}]`, ...data)),
+    error: (...data: any[]) => console.error(chalk.bgRedBright(`[ERR:${name.toUpperCase()}]`, ...data)),
+    success: (...data: any[]) => console.log(chalk.greenBright(`[CHK:${name.toUpperCase()}]`,...data)),
   }
 }
