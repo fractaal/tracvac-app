09162a648bf641c4f215e1eaa58456c8bcd91984
fractaal <sonoroustwo@gmail.com>
2021-03-09 01:16:46 +0800

    ✨ snake case mappers

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,12 +1,12 @@
 import Logger from '../logger';
 import { dbStatusCheck } from './migration';
-import { Model } from 'objection';
+import { Model, snakeCaseMappers } from 'objection';
 import Knex from 'knex';
 
 const logger = Logger('Database');
 
 // Init knex
-logger.log('Initializing Knex...')
+logger.log('Initializing SQL query builder...')
 export const knex = Knex({
   client: 'pg',
   useNullAsDefault: true,
@@ -16,10 +16,11 @@ export const knex = Knex({
     port     : 35432,
     password : 'postgres',
     database : 'vaccinePassportDb',
-}});
+  },
+  ...snakeCaseMappers()
+});
 
 export async function connect() {
   logger.log('Connecting to database..');
-
   await dbStatusCheck();
 }
