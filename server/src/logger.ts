cdb3c044a98889aa8a17cba8d0598c589e462bdd
fractaal <sonoroustwo@gmail.com>
2021-03-09 01:16:34 +0800

    🔧 further shorten prefixes

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,11 +1,11 @@
 import chalk from 'chalk';
 
 export default function register (name: string) {
-  console.log(chalk.blue(`[NEW:Logger] Registered new subsystem ${name}.`));
+  console.log(chalk.blue(`[Logger] Registered new module ${name}.`));
   return {
-    log: (...data: any[]) => console.log(`[INF:${name.toUpperCase()}]`, ...data),
-    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[WRN:${name.toUpperCase()}]`, ...data)),
-    error: (...data: any[]) => console.error(chalk.bgRedBright(`[ERR:${name.toUpperCase()}]`, ...data)),
-    success: (...data: any[]) => console.log(chalk.greenBright(`[CHK:${name.toUpperCase()}]`,...data)),
+    log: (...data: any[]) => console.log(`[I:${name.toUpperCase()}]`, ...data),
+    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[W:${name.toUpperCase()}]`, ...data)),
+    error: (...data: any[]) => console.error(chalk.bgRedBright(`[E:${name.toUpperCase()}]`, ...data)),
+    success: (...data: any[]) => console.log(chalk.greenBright(`[S:${name.toUpperCase()}]`,...data)),
   }
 }
