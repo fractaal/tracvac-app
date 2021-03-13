3a9a8b0da2e72b741dfd8265e7740c60884c7a21
fractaal <sonoroustwo@gmail.com>
2021-03-13 12:47:32 +0800

    ➕add transition animations, logic

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="q-app">
-    <router-view />
+    <transition :enter-active-class="`animated ${transition.in}`" :leave-active-class="`animated ${transition.out}`" mode="out-in" :duration="300">
+      <router-view/>
+    </transition>
   </div>
 </template>
 <script lang="ts">
+import transition from './transitions'
+import { recept } from './api/server'
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      transition
+    }
+  },
+  async created () {
+    // Show splash screen
+    await this.$router.push('/')
+
+    // Check whether or not user has already set an address to connect to
+    const server: string|null = this.$q.localStorage.getItem('server')
+
+    // Artificial delay to show the logo
+    await new Promise((resolve) => setTimeout(resolve, 1500))
+
+    if (server) {
+      // Connect to the server and go to the login screen.
+      await recept(server)
+      await this.$router.push('/login')
+    } else {
+      // Go to the server connecet screen otherwise
+      await this.$router.push('/connect')
+    }
+    NavigationBar.show()
+  }
 })
 </script>
