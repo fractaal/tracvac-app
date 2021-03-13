1e38ab65fc6f92090a53380915a8d5385c6732e9
fractaal <sonoroustwo@gmail.com>
2021-03-13 12:48:37 +0800

    ✨ authentication logic

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,4 +1,25 @@
-export async function login (username: string, password: string) {
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { api } from './server'
+
+let authed = false
+
+export function isAuthed () { return authed }
+
+export async function login (username: string, password: string): Promise<[boolean, string?]> {
   console.log(`Logging in with ${username} and ${password}...`)
-  return new Promise(resolve => setTimeout(() => { resolve(true) }, 2500))
+
+  try {
+    const response = await api.post('/login', { username, password })
+
+    if (response.data.result) {
+      authed = true
+      return [true]
+    } else {
+      authed = false
+      return [false, response.data.message as unknown as string]
+    }
+  } catch (err) {
+    authed = false
+    return [false, 'An error happened while logging you in.']
+  }
 }
