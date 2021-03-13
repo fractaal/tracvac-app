c8b47fab3d95d4c30d45f1038d488b0385609279
fractaal <sonoroustwo@gmail.com>
2021-03-13 13:14:57 +0800

    ✨ typedefs

---
+++ client/src/globals.d.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-var */
+declare module 'http-proxy-middleware';
+
+interface Window {
+  NavigationBar: NavigationBar
+}
+
+interface NavigationBar {
+  isVisible: boolean;
+  backgroundColorByName: (colorname: string, lightNavigationBar: boolean) => void;
+  backgroundColorByHexString: (hexString: string, lightNavigationBar: boolean) => void;
+  hide: () => void;
+  show: () => void;
+}
+
+declare var NavigationBar: NavigationBar
