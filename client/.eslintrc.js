3b7901c656102534927a4d435ac32ab8cff42beb
fractaal <sonoroustwo@gmail.com>
2021-03-13 12:44:34 +0800

    🔧 turn off no-undef eslint rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -72,6 +72,7 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 'off',
     'one-var': 'off',
+    'no-undef': 'off',
 
     'import/first': 'off',
     'import/named': 'error',
