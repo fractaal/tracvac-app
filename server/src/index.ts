f8ce5d23048228f8a35636ea6d4f2e77f28dae7a
fractaal <sonoroustwo@gmail.com>
2021-03-17 23:42:37 +0800

    ➕ functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,68 @@
 const start = Date.now();
 
-import express, { Request, Response } from 'express';
+// Constants
+import { port, host } from './constants';
+import { secret } from './secret';
+
+// Libraries
+import express, { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import path from 'path';
 import Logger from './logger';
-import bodyParser from 'body-parser';
 import * as database from './database';
+
+// Middleware
+import { json } from 'body-parser';
+import cors from 'cors';
+import { TokenData } from './interfaces/TokenData';
 
 const logger = Logger('Main');
 export const app = express();
 
-// Use bodyParser
-app.use(bodyParser.json());
+// Use middleware
+app.use(cors());
+app.use(json());
+
+logger.log("Initializing JWT middleware"); 
+
+app.use(function (req, res, next) {
+  const token = req.header('x-access-token');
+  if (!token) {
+    req.isAuthenticated = false;
+  } else {
+    jwt.verify(token, secret, (error, decoded) => {
+      if (error) {
+        logger.warn('Error occured whilst trying to verify user JWT: ', error);
+        req.isAuthenticated = false
+      } else {
+        req.isAuthenticated = true
+        req.tokenData = decoded as Request['tokenData']
+      }
+    })
+  }
+  next();
+});
+
 
 (async () => {
   logger.log("Initializing database...");
   await database.connect();
 
+  // API endpoints are stored in the routes folder. This should auto 
   logger.log("Initializing API endpoints...")
   const routePath = path.join(__dirname, "/routes");
   for (const filename of fs.readdirSync(routePath)) await import(path.join(routePath, path.basename(filename)));
 
-  app.use((err: Error, _: Request, res: Response) => {
+  // Error handling.
+  logger.log('Initializing error handler middleware');
+  app.use(function (err: Error, _: Request, res: Response, __: NextFunction) {
     logger.error(err.stack);
     res.status(500).json({result: false, message: 'Something happened with us while processing your request. Sorry!'});
   })
   
-  logger.log("Binding to port...")
-  app.listen(3000);
+  logger.log(`Binding to port ${port} on ${host}...`)
+  app.listen(port, host)
   
   logger.success("Server start complete! Startup took " + (Date.now() - start) + "ms");
 })();
