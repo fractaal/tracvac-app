4f07c9356fcf8850bc74acb1445f7e6329ef2d53
fractaal <sonoroustwo@gmail.com>
2021-03-17 23:58:57 +0800

    ➕conditional statusbar / navbar

--- client/src/pages/Splash.vue
+++ client/src/pages/Splash.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="column content-center bg-blue-900" key="splash">
+  <q-page class="column content-center bg-blue-900">
     <img src="~assets/tracvac-logo.png" class="w-1/2 mt-44 bobbing bobbing-fast" alt="" />
     <div class="text-4xl text-center logo-text text-white">Tracvac</div>
   </q-page>
@@ -11,10 +11,12 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'Splash',
   created () {
-    StatusBar.styleLightContent()
-    StatusBar.backgroundColorByHexString('#1e3a8a')
-    NavigationBar.hide()
-    NavigationBar.backgroundColorByHexString('#1e3a8a', false)
+    if (window.StatusBar && window.NavigationBar) {
+      StatusBar.styleLightContent()
+      StatusBar.backgroundColorByHexString('#1e3a8a')
+      NavigationBar.hide()
+      NavigationBar.backgroundColorByHexString('#1e3a8a', false)
+    }
   }
 })
 </script>
