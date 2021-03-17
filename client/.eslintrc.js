2c5750a336dfa10c846634897c0bb54ebac0c455
fractaal <sonoroustwo@gmail.com>
2021-03-17 23:54:15 +0800

    ❌ no-floating-promises rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -90,6 +90,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
