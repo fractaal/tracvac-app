da20136a08e696068fb920ace997a90bc130913f
fractaal <sonoroustwo@gmail.com>
2021-03-17 23:55:50 +0800

    ➕ functionality

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { api } from './server'
+import { LocalStorage } from 'quasar'
 
 let authed = false
 
@@ -12,6 +13,7 @@ export async function login (username: string, password: string): Promise<[boole
     const response = await api.post('/login', { username, password })
 
     if (response.data.result) {
+      LocalStorage.set('token', response.data.token)
       authed = true
       return [true]
     } else {
@@ -20,6 +22,42 @@ export async function login (username: string, password: string): Promise<[boole
     }
   } catch (err) {
     authed = false
-    return [false, 'An error happened while logging you in.']
+    return [false, `A clientside error happened while logging you in: ${err}`]
+  }
+}
+
+export async function loginWithToken (): Promise<boolean> {
+  const token = LocalStorage.getItem('token')
+  if (token) {
+    // Check with the API if the token is actually valid
+    api.defaults.headers = {
+      'X-Access-Token': token
+    }
+    const response = await api.get('/is-authorized')
+    if (response.data.result === true) {
+      authed = true
+      return true
+    }
+  }
+  return false
+}
+
+export async function register (formData: Record<string, any>): Promise<[boolean, string?]> {
+  console.log('Registering...')
+
+  try {
+    const response = await api.post('/user', formData)
+
+    if (response.data.result) {
+      authed = true
+      return [true]
+    } else {
+      authed = false
+      return [false, response.data.message as unknown as string]
+    }
+  } catch (err) {
+    authed = false
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    return [false, `A clientside error happened while trying to register you: ${err.message}`]
   }
 }
--- client/src/api/server.ts
+++ client/src/api/server.ts
@@ -1,7 +1,12 @@
-import axios, { AxiosInstance } from 'axios'
+import axios from 'axios'
 import { Notify } from 'quasar'
 
-export let api: AxiosInstance
+export const api = axios.create({
+  timeout: 5000,
+  baseURL: ''
+})
+
+export let server: string
 
 function addHttp (url: string) {
   if (!/^(?:f|ht)tps?:\/\//.test(url)) {
@@ -13,27 +18,31 @@ function addHttp (url: string) {
 export async function recept (address: string): Promise<boolean> {
   try {
     address = addHttp(address.trim())
-    const possibleApi = axios.create({ baseURL: address, timeout: 5000 })
-    const response = await possibleApi.get('/reception')
+    const response = await api.get(address + '/reception')
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (response.data?.version) {
       Notify.create({
         type: 'positive',
         message: `Connected to ${address}!`
       })
-      api = possibleApi
+      api.defaults.baseURL = address
+      server = address
       return true
     } else {
       Notify.create({
         type: 'negative',
-        message: `${address} doesn't seem to be a valid Tracvac site.`
+        message: `${address} doesn't seem to be a valid Tracvac site.`,
+        timeout: 2500,
+        position: 'center'
       })
       return false
     }
   } catch (err) {
     Notify.create({
       type: 'negative',
-      message: `An error happened while connecting: ${(err as unknown as Error).message}`
+      message: `An error happened while connecting: ${(err as unknown as Error).message}`,
+      timeout: 2500,
+      position: 'center'
     })
     return false
   }
