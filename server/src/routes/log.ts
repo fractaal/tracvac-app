491cd18f3c14eb11b4cb529b93e6bee27545c518
fractaal <sonoroustwo@gmail.com>
2021-03-17 23:44:50 +0800

    ➕ log route

---
+++ server/src/routes/log.ts
@@ -0,0 +1,41 @@
+import { app } from '../';
+import Logger from '../logger';
+import { LogModel } from '../database/models/LogModel';
+
+const logger = Logger('Log-Route')
+
+app.use('/log', async (request, response, next) => {
+  if (request.isAuthenticated) {
+    if (request.tokenData.userId) {
+      next();
+    } else {
+      response.status(400).json({ result: false, message: 'Please reauthenticate' })
+    }
+  } else {
+    response.status(400).json({ result: false, message: 'You are not authenticated' })
+  }
+})
+
+app.get('/log', async (request, response, next) => {
+  try {
+    const logs = await LogModel.query()
+      .select('*')
+      .where('userId', '=', request.tokenData.userId)
+      .orderBy('createdAt');
+
+    response.status(200).json({ result: true, logs })
+  } catch (err) { next(err); }
+})  
+
+app.post('/log', async (request, response, next) => {
+  try {
+    try {
+      await LogModel.query().insert({ ...request.body, userId: request.tokenData.userId })
+    } catch (err) {
+      response.status(400).json({ result: false, message: 'You sent invalid data.' })
+      return;
+    }
+    logger.log(`Log by user ${request.tokenData.userId} posted!`)
+    response.json({ result: true, message: 'Log posted!' })
+  } catch (err) { next(err); }
+})
