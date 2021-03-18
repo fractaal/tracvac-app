ab275cd4bd75bee11ce222ad5232ddf066da9b6b
fractaal <sonoroustwo@gmail.com>
2021-03-18 22:29:24 +0800

    ➕ Vaccine Status page

---
+++ client/src/pages/VaccineStatus.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="px-8">
+    <h5 class="font-bold mt-8 p-0">Vaccine Status</h5>
+    <div class="p-8 mb-8 border-solid border-blue-500 shadow-lg rounded-lg text-center">
+      <q-icon class="block mx-auto" :name="store.userInfo.isVaccinated ? 'fas fa-check' : 'fas fa-times'" size="196px"/>
+      <p class="m-0 font-bold">Your vaccine is {{store.userInfo.isVaccineReady.toLowerCase()}}.</p>
+      <i>Check back soon! We'll notify you once it is.</i>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { store } from '../api/store'
+import Vue from 'vue'
+
+import ButtonCard from '../components/ButtonCard.vue'
+
+export default Vue.extend({
+  name: 'Profile',
+  components: { ButtonCard },
+  data () {
+    return {
+      store
+    }
+  }
+})
+</script>
