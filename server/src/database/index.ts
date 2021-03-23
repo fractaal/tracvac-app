8825816ff4ac930b48025e563c74eeeae7d2c39b
fractaal <sonoroustwo@gmail.com>
2021-03-23 13:20:03 +0800

    ♻ postgresql to sqlite

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -6,6 +6,7 @@ const logger = Logger('Database');
 
 // Init knex
 logger.log('Initializing SQL query builder...')
+/**
 export const knex = Knex({
   client: 'pg',
   useNullAsDefault: true,
@@ -16,7 +17,14 @@ export const knex = Knex({
     password : 'postgres',
     database : 'tracvac-db',
   },
-});
+});*/
+
+export const knex = Knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './tracvac-db.sqlite3'
+  }
+})
 
 export async function connect() {
   logger.log('Connecting to database..');
