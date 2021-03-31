63b24ea8a45ae32a20244469bb4fa26a58af50ea
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:28:25 +0800

    ➕ personal info component

---
+++ client/src/pages/PersonalInfo.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PersonalInfo',
+  data () {
+    return {
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
