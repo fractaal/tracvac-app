57f3deedd1ae9273ae9254f923c8a9bc4e0f22b1
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:03:09 +0800

    ➕ populate response data

--- server/src/routes/reception.ts
+++ server/src/routes/reception.ts
@@ -1,16 +1,18 @@
 import Logger from '../logger';
 import { app } from '../';
+import {getConfig} from "../config";
 
 const logger = Logger('Reception-Route');
 
-app.get('/reception', (_, res) => {
+app.get('/reception', async (req, res) => {
   try {
+    const config = await getConfig()
     res.json({
       version: require('../../package.json').version,
-      name: `Test`,
-      description: `Description.`,
-    }); 
+      location: config.location,
+      lguUrl: config.lguUrl
+    });
   } catch(err) {
-    logger.error(`Something happened while trying to send ${_.ip} reception data - ${err.stack}`);
+    logger.error(`Something happened while trying to send ${req.ip} reception data - ${err.stack}`);
   }
 })
