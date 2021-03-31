0300ec85384a5d58d4546af16089f7d2c6b3b2db
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:18:01 +0800

    ➕ profile picture component

---
+++ client/src/components/ProfilePicture.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="block rounded-full shadow-xl ring-4 ring-white" :src="fullPath" alt="">
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { store } from 'src/api/store'
+import path from 'path'
+import { LocalStorage } from 'quasar'
+
+export default Vue.extend({
+  name: 'ProfilePicture',
+  created () {
+    this.fullPath = new URL(store.userInfo?.profilePicturePath, LocalStorage.getItem('server'))
+  },
+  data () {
+    return {
+      store,
+      fullPath: ''
+    }
+  },
+  watch: {
+    'store.userInfo.profilePicturePath': function () {
+      this.fullPath = new URL(store.userInfo?.profilePicturePath, LocalStorage.getItem('server'))
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
