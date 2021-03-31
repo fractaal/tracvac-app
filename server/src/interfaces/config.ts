2a0db72cf208602a0d2a68ae3d58d97b1b186e63
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:01:15 +0800

    ➕ config interface

---
+++ server/src/interfaces/config.ts
@@ -0,0 +1,20 @@
+export default interface Config {
+
+    /**
+     * Location of the Tracvac site.
+     */
+    location: string;
+
+    /**
+     * Link to the LGU's website
+     */
+    lguUrl: string;
+
+    /**
+     * Secret key.
+     */
+    secret: string;
+
+    httpPort: number;
+    httpsPort: number;
+}
