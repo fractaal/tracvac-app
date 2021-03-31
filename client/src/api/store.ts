65d2220ad1a94f6c41269a4dd4f9e7757d2035e8
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:12:45 +0800

    ➕ items to store

--- client/src/api/store.ts
+++ client/src/api/store.ts
@@ -1,7 +1,13 @@
 import { Log } from './logs'
 import { User } from './user'
+import { MenuItem } from 'src/api/menu'
+import { Notification } from 'src/api/notification'
 
 export const store = {
-  userInfo: {} as unknown as User,
-  logs: [] as Log[]
+  userInfo: {} as unknown as User | null,
+  logs: [] as Log[],
+  recentlyViewed: {} as MenuItem,
+  activeRoute: '',
+  notifications: [] as Notification[],
+  serverInfo: {} as Record<string, string>
 }
