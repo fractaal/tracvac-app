1946d65bf821634527261a387de91a732b7e37cf
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:15:03 +0800

    ➕ large button card component

---
+++ client/src/components/LargeButtonCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-ripple @click="emitClick" class="relative border border-solid border-gray-200 p-4 rounded-lg">
+    <p class="m-0 font-bold text-h6">{{title}}</p>
+    <p class="m-0">{{description}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LargeButtonCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    emitClick () {
+      setTimeout(() => { this.$emit('click') }, 150)
+    }
+  }
+})
+
+</script>
+
+<style>
+
+</style>
