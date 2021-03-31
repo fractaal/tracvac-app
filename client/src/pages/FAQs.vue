ad7b20c281124eaa527a641f03effe82e8b5e964
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:21:28 +0800

    ➕ faqs

---
+++ client/src/pages/FAQs.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page>
+    <custom-header title="FAQs"/>
+  </q-page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import CustomHeader from 'components/CustomHeader.vue'
+
+export default Vue.extend({
+  name: 'FAQs',
+  components: { CustomHeader }
+})
+</script>
+
+<style scoped>
+
+</style>
