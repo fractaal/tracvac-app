3a6ac357d523f091ea6569ccdf44a1dccf390433
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:13:11 +0800

    ➕ profile picture upload functionality

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,5 +1,8 @@
 import { api } from './server'
 import { store } from './store'
+import notify from 'src/api/notify'
+import { logout } from 'src/api/auth'
+import { FormData } from 'src/templates/registrationFormTemplate'
 
 interface UserInfoResponse {
   result: boolean;
@@ -11,6 +14,7 @@ export interface User {
   username: string;
   email: string;
   createdAt: string;
+  profilePicturePath: string;
   updatedAt?: any;
   isVaccinated: boolean;
   isVaccineReady: string;
@@ -50,12 +54,59 @@ export interface User {
 }
 
 export async function getUserInfo (): Promise<boolean> {
-  const response = await (await api.get('/user')).data as unknown as UserInfoResponse
+  try {
+    const response = await (await api.get('/user')).data as unknown as UserInfoResponse
 
-  if (response.result) {
-    store.userInfo = Object.assign({}, store.userInfo, response.user)
-    return true
+    if (response.result) {
+      store.userInfo = Object.assign({}, store.userInfo, response.user)
+      return true
+    }
+
+    return false
+  } catch (e) {
+    console.warn('User info retrieval failed! Token must be invalid! Logging out...')
+    notify.negative('Please log in again.')
+    logout(true)
+    return false
   }
+}
 
-  return false
+export async function setUserInfo (data: FormData): Promise<boolean> {
+  try {
+    const response = await api.patch('/user', data)
+
+    if (response.data.result) {
+      await getUserInfo()
+      notify.positive('Personal information updated!')
+      return true
+    } else {
+      notify.negative(`Personal information not saved: ${response.data.message}`)
+      return false
+    }
+  } catch (e) {
+    notify.negative(`Personal information not saved: ${e}`)
+    return false
+  }
+}
+
+export async function uploadProfilePicture (file: File) {
+  const formData = new FormData()
+
+  formData.append('file', file)
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const response = (await api.post('/upload-profile-picture', formData, { headers: { 'Content-Type': 'multipart/form-data' } })).data
+    if (response.result) {
+      notify.positive('Profile picture changed!')
+      await getUserInfo()
+      return true
+    } else {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      notify.negative(`Profile picture change failed: ${response.message}`)
+      return false
+    }
+  } catch (err) {
+    notify.negative(`Profile picture upload failed: ${(err as unknown as Error).message}`)
+    return false
+  }
 }
