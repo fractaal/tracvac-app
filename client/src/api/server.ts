ed0b68832f1365bff95cc93c5244fe517c4985c9
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:12:27 +0800

    ➕ complicated recept/disconnect logic

--- client/src/api/server.ts
+++ client/src/api/server.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
-import { Notify } from 'quasar'
+import notify from 'src/api/notify'
+import { store } from 'src/api/store'
+import { Dialog, LocalStorage } from 'quasar'
+import { logout } from 'src/api/auth'
+import { routerInstance } from 'boot/router'
+import { menuLayout } from 'src/api/menu'
 
 export const api = axios.create({
   timeout: 5000,
@@ -8,7 +13,7 @@ export const api = axios.create({
 
 export let server: string
 
-function addHttp (url: string) {
+export function addHttp (url: string) {
   if (!/^(?:f|ht)tps?:\/\//.test(url)) {
     url = 'http://' + url
   }
@@ -21,29 +26,44 @@ export async function recept (address: string): Promise<boolean> {
     const response = await api.get(address + '/reception')
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (response.data?.version) {
-      Notify.create({
-        type: 'positive',
-        message: `Connected to ${address}!`
-      })
+      console.log('reception', response.data)
+      for (const key in response.data) {
+        LocalStorage.set(key, response.data[key])
+        store.serverInfo = Object.assign({}, store.serverInfo, response.data)
+      }
+      store.serverInfo = Object.assign({}, store.serverInfo, { address })
+      notify.positive(`Connected to ${response.data.location}!`)
+      LocalStorage.set('server', address)
       api.defaults.baseURL = address
       server = address
+
+      // Show / hide LGU URL button depending on whether or not lguUrl is present
+      if (!(response.data.lguUrl)) {
+        for (let i = 0; i < menuLayout.length; i++) {
+          if (menuLayout[i].name === 'Go to the LGU website') {
+            menuLayout[i].hidden = true
+          }
+        }
+      }
       return true
     } else {
-      Notify.create({
-        type: 'negative',
-        message: `${address} doesn't seem to be a valid Tracvac site.`,
-        timeout: 2500,
-        position: 'center'
-      })
+      notify.negative(`${address} doesn't seem to be a valid Tracvac site.`)
       return false
     }
   } catch (err) {
-    Notify.create({
-      type: 'negative',
-      message: `An error happened while connecting: ${(err as unknown as Error).message}`,
-      timeout: 2500,
-      position: 'center'
-    })
+    notify.negative(`An error happened while connecting ${(err as unknown as Error).message}`)
     return false
   }
+}
+
+export async function disconnect () {
+  Dialog.create({
+    title: `Disconnect from ${store.serverInfo.location}?`,
+    message: 'By disconnecting, you\'ll be logged out and taken back to the site selection screen.',
+    ok: true,
+    cancel: true
+  }).onOk(() => {
+    logout(true)
+    routerInstance.push('/connect')
+  })
 }
