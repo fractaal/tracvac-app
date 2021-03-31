133102b8baa33f696faf7ea1f553a4de6a1898cd
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:29:37 +0800

    ➕ preferences screen

---
+++ client/src/pages/Preferences.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page>
+    <custom-header title="Preferences"/>
+    <div class="px-8">
+      <p class="font-bold m-0 p-0 text-gray-500">YOU'RE CONNECTED TO</p>
+      <p class="text-h5 m-0">{{store.serverInfo.location}}</p>
+      <p class="m-0">at <b>{{store.serverInfo.address}}</b></p>
+      <hr>
+      <q-btn outline label="Disconnect" @click="disconnect"/>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { store } from 'src/api/store'
+import { disconnect } from 'src/api/server'
+import CustomHeader from 'components/CustomHeader.vue'
+
+export default Vue.extend({
+  name: 'Preferences',
+  components: { CustomHeader },
+  data () {
+    return {
+      store
+    }
+  },
+  methods: { disconnect }
+})
+</script>
+
+<style scoped>
+
+</style>
