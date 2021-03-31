a3727614ad45ebbe6f88476285819b5419f9a97e
fractaal <sonoroustwo@gmail.com>
2021-03-31 17:18:26 +0800

    ➕ branding

--- client/src/extensions/tailwindcss/tailwind.config.js
+++ client/src/extensions/tailwindcss/tailwind.config.js
@@ -2,9 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        blue: {
+          100: '#E2F0F8',
+          400: '#7AC3E6',
+          500: '#3D99CE'
+        }
+      }
+    }
   },
-
   variants: {},
   plugins: []
 }
