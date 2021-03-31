b92d2594aa3d78659638f5eb4ea6e1f0cd5bdb72
fractaal <sonoroustwo@gmail.com>
2021-03-31 16:59:37 +0800

    ➖ unused constants

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,17 @@
 const start = Date.now();
 
 // Constants
-import { port, host } from './constants';
-import { secret } from './secret';
 
 // Libraries
+import http from 'http';
+import https from 'https';
+import open from 'open';
 import express, {NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import path from 'path';
 import Logger from './logger';
+import { getConfig } from "./config";
 import * as database from './database';
 
 // Middleware
@@ -20,8 +22,7 @@ import cors from 'cors';
 const logger = Logger('Main');
 export const app = express();
 export const staticPath = path.resolve(process.cwd(), 'static');
-
-logger.log(`Static files path: ${staticPath}`)
+export const internalStaticPath = path.resolve(__dirname, '../', 'static')
 
 // Use middleware
 app.use(cors());
@@ -31,39 +32,59 @@ app.use(fileUpload({
 }))
 
 // Static
+if (!fs.existsSync(staticPath)) fs.mkdirSync(staticPath);
+
+const blocker = (_: Request, res: Response) => {
+  res.status(401).send("You are not authorized");
+}
+
+app.get('/index.html', blocker);
+app.get('/', blocker);
+
 app.use(express.static(staticPath));
-app.use(express.static(path.resolve(__dirname, '..', 'static')));
+app.use(express.static(internalStaticPath));
 
-// JWT Middleware
-logger.log("Initializing JWT middleware");
-app.use(function (req, res, next) {
-  const token = req.header('x-access-token');
-  if (!token) {
-    req.isAuthenticated = false;
-  } else {
-    jwt.verify(token, secret, (error, decoded) => {
-      if (error) {
-        logger.warn('Error occured whilst trying to verify user JWT: ', error);
-        req.isAuthenticated = false
-      } else {
-        req.isAuthenticated = true
-        req.tokenData = decoded as Request['tokenData']
-      }
-    })
-  }
-  next();
-});
-
-// HTTPS middleware
-app.use((req, _, next) => {
-  if (!req.secure) {
-    logger.warn(`This request (${req.ip}) is not secure. Sensitive data such as login credentials or access tokens can be intercepted.
-    To silence this warning, access your administrative interface at https://localhost:6960 or turn it off in settings.json.`)
-  }
-  next()
-});
+logger.log(`Internal static files path: ${internalStaticPath}`);
+logger.log(`Static files path: ${staticPath}`);
 
 (async () => {
+  logger.log("Reading configuration...");
+  const config = await getConfig();
+
+  if (!config) {
+    await open(`http://localhost:${(await getConfig())?.httpPort ?? 80}/admin`);
+  }
+
+  // JWT Middleware
+  logger.log("Initializing JWT middleware");
+  app.use(async (req, res, next) => {
+    const token = req.header('x-access-token');
+    if (!token) {
+      req.isAuthenticated = false;
+    } else {
+      jwt.verify(token, (await getConfig()).secret, (error, decoded) => {
+        if (error) {
+          logger.warn('Error occurred whilst trying to verify user JWT: ', error);
+          req.isAuthenticated = false
+        } else {
+          req.isAuthenticated = true
+          req.tokenData = decoded as Request['tokenData']
+        }
+      })
+    }
+    next();
+  });
+
+  /**
+  // HTTPS middleware
+  app.use(async (req, _, next) => {
+    if (!req.secure) {
+      logger.warn(`This request (${req.ip}) is not secure. Sensitive data such as login credentials or access tokens can be intercepted.
+      To silence this warning, access your administrative interface at http://localhost:${(await getConfig())?.httpPort ?? 80}/admin or turn it off in settings.json.`)
+    }
+    next()
+  });*/
+
   logger.log("Initializing database...");
   await database.connect();
 
@@ -79,8 +100,22 @@ app.use((req, _, next) => {
     res.status(500).json({result: false, message: 'Something happened with us while processing your request. Sorry!'});
   })
 
-  logger.log(`Binding to port ${port} on ${host}...`)
-  app.listen(port, host)
+  // HTTPS / HTTP server starts
+  logger.log(`HTTP - Binding to port ${config?.httpPort ?? 80}.`)
+  http.createServer(app).listen(config?.httpPort ?? 80).on('error', (err) => {
+    logger.error(`Failed to bind to port ${config?.httpPort ?? 80}: ` + err);
+  });
+
+
+  if (fs.existsSync('key.pem') && fs.existsSync('cert.pem')) {
+    logger.log(`HTTPS certificates detected! Binding to port ${config?.httpsPort ?? 443}.`)
+    https.createServer({
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    }, app).listen(config?.httpsPort ?? 443).on('error', (err) => {
+      logger.error(`Failed to bind to port ${config?.httpsPort ?? 443}: ` + err);
+    });
+  }
 
   logger.success("Server start complete! Startup took " + (Date.now() - start) + "ms");
 })();
