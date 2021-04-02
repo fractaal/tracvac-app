ce3e017588d24574c5d8fb50f893933df9e77260
fractaal <sonoroustwo@gmail.com>
2021-04-02 20:43:12 +0800

    ✨ userShownInLogs key

--- admin/src/api/store.ts
+++ admin/src/api/store.ts
@@ -3,6 +3,7 @@ import { AxiosInstance } from 'axios';
 export default {
   usersToModify: [] as Record<string,any>[],
   axios: null as unknown as AxiosInstance,
+  userShownInLogs: null as unknown as Record<string,any>,
   serverConfig: {
     isConfigured: false,
     secret: null,
