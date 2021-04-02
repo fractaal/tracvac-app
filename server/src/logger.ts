85dd07637be964f86170483bfc09c1bf9ffea76f
fractaal <sonoroustwo@gmail.com>
2021-04-03 00:40:14 +0800

    ✨ clustering

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,11 +1,14 @@
 import chalk from 'chalk';
+import cluster from 'cluster';
 
 export default function register (name: string) {
   console.log(chalk.blue(`[Logger] Registered new module ${name}.`));
+  const clusterName = cluster.isWorker ? `#${cluster.worker.id}` : `MAIN`;
+
   return {
-    log: (...data: any[]) => console.log(`[I:${name.toUpperCase()}]`, ...data),
-    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[W:${name.toUpperCase()}]`, ...data)),
-    error: (...data: any[]) => console.error(chalk.bgRedBright(`[E:${name.toUpperCase()}]`, ...data)),
-    success: (...data: any[]) => console.log(chalk.greenBright(`[S:${name.toUpperCase()}]`,...data)),
+    log: (...data: any[]) => console.log(`[${clusterName} I:${name.toUpperCase()}]`, ...data),
+    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[${clusterName} W:${name.toUpperCase()}]`, ...data)),
+    error: (...data: any[]) => console.error(chalk.bgRedBright(`[${clusterName} E:${name.toUpperCase()}]`, ...data)),
+    success: (...data: any[]) => console.log(chalk.greenBright(`[${clusterName} S:${name.toUpperCase()}]`,...data)),
   }
 }
