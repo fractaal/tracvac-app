062c90f547ab4d559987a60b13b0ff110760a5d2
fractaal <sonoroustwo@gmail.com>
2021-04-03 21:22:54 +0800

    ✨ don't throw error if status is less than 500

--- client/src/boot/axios.ts
+++ client/src/boot/axios.ts
@@ -1,6 +1,11 @@
 import axios, { AxiosInstance } from 'axios'
 import { boot } from 'quasar/wrappers'
 
+axios.defaults.validateStatus = (number) => {
+  console.log(`Status is ${number < 500}`)
+  return number < 500
+}
+
 declare module 'vue/types/vue' {
   interface Vue {
     $axios: AxiosInstance;
