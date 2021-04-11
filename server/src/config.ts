c5bfae7983a7ea2e8d4a1c074570c7196b7a26ae
fractaal <sonoroustwo@gmail.com>
2021-04-11 22:25:11 +0800

    ✨ fallback values, isProperlyConfigured function

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,9 +6,29 @@ const logger = Logger('Config')
 
 export async function getConfig(): Promise<Config> {
     if (typeof Storage.get !== 'function') {
-        await Storage.init({dir: 'tracvac-config'}).then(() => logger.success('Configuration loaded!'));
+        await Storage.init({dir: 'tracvac-config'})
+        logger.success('Configuration loaded!');
     }
-    return await Storage.get('config') as Config;
+
+    const config = await Storage.get('config') as Config;
+
+    return {
+        location: process.env.TV_LOCATION ?? config?.location,
+        lguUrl: process.env.TV_LGUURL ?? config?.lguUrl,
+        secret: process.env.TV_SECRET ?? config?.secret,
+        httpsPort: process.env.TV_PORT_SECURE ?? config?.httpsPort ?? 443,
+        httpPort: process.env.TV_PORT ?? config?.httpPort ?? 80
+    } as Config;
+}
+
+export async function isProperlyConfigured(): Promise<boolean> {
+    const config = await getConfig() as Partial<Config>;
+
+    return typeof config.httpPort === 'string' &&
+        typeof config.httpsPort === 'string' &&
+        typeof config.secret === 'string' &&
+        typeof config.lguUrl === 'string' &&
+        typeof config.location === 'string';
 }
 
 export async function setConfig(config: Config): Promise<boolean> {
