72b0329e601493a6cb974590bf1dd25351c75bbb
fractaal <sonoroustwo@gmail.com>
2021-04-11 22:29:58 +0800

    ✨ verbosity option, formatting change

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -2,13 +2,24 @@ import chalk from 'chalk';
 import cluster from 'cluster';
 
 export default function register (name: string) {
-  console.log(chalk.blue(`[Logger] Registered new module ${name}.`));
   const clusterName = cluster.isWorker ? `#${cluster.worker.id}` : `MAIN`;
 
+  if (process.env.VERBOSE == '1') console.log(chalk.blue(`[${clusterName}|LOGGER] Registered new module ${name}.`));
+
   return {
-    log: (...data: any[]) => console.log(`[${clusterName} I:${name.toUpperCase()}]`, ...data),
-    warn: (...data: any[]) => console.warn(chalk.yellowBright(`[${clusterName} W:${name.toUpperCase()}]`, ...data)),
-    error: (...data: any[]) => console.error(chalk.bgRedBright(`[${clusterName} E:${name.toUpperCase()}]`, ...data)),
-    success: (...data: any[]) => console.log(chalk.greenBright(`[${clusterName} S:${name.toUpperCase()}]`,...data)),
+    log: (...data: any[]) => {
+      if (process.env.VERBOSE == '1') {
+        console.log(`[${clusterName}|I:${name.toUpperCase()}]`, ...data)
+      }
+    },
+    warn: (...data: any[]) => {
+      console.warn(chalk.yellowBright(`[${clusterName}|W:${name.toUpperCase()}]`, ...data))
+    },
+    error: (...data: any[]) => {
+      console.error(chalk.bgRedBright.black(`[${clusterName}|E:${name.toUpperCase()}]`, ...data))
+    },
+    success: (...data: any[]) => {
+      console.log(chalk.greenBright(`[${clusterName}|S:${name.toUpperCase()}]`,...data))
+    },
   }
 }
