f1d5079e94e155f8ef5b387c11835bb84317a2af
fractaal <sonoroustwo@gmail.com>
2021-04-12 15:45:06 +0800

    ✨ modify to use route params

--- admin/src/pages/ViewLogs.vue
+++ admin/src/pages/ViewLogs.vue
@@ -1,41 +1,50 @@
 <template>
   <q-page class='p-8'>
     <h4 class='m-0 font-light'>LOGS</h4>
-    <p>For {{store.userShownInLogs.firstName}} {{store.userShownInLogs.middleName}} {{store.userShownInLogs.lastName}}</p>
-    <hr>
-    <div v-if='logs.length !== 0'>
-      <div class='grid grid-cols-2 gap-4'>
-        <q-card class='mb-2 shadow-3' v-for='log in logs' :key='log.createdAt'>
-          <q-card-section>
-            <div class='font-bold'>
-              Created {{formatDateAndTime(log.createdAt).formatted}}
-              <br>
-              {{formatDateAndTime(log.createdAt).formattedDistance}}
-            </div>
-            <div class='grid grid-cols-2' v-for='(displayName, name) in displayNameMappings' :key='name'>
-              <p class='p-0 m-0' >
-                {{displayName}}
-              </p>
-              <div v-if='!log[name]' class='text-green-500'>
-                <q-icon name='fas fa-check-circle'/>
-                No
-              </div>
-              <div v-else class='text-red-500'>
-                <q-icon name='fas fa-exclamation-triangle'/>
-                Yes
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
+    <p v-if='activeUser'>For {{activeUser.firstName}} {{activeUser.middleName}} {{activeUser.lastName}}</p>
+    <q-spinner v-else/>
+    <hr class='mb-4'>
+    <div class='grid grid-cols-2 gap-4'>
+      <div>
+        <div v-if='logs.length !== 0'>
+          <div>
+            <q-card class='mb-2 shadow-3' v-for='log in logs' :key='log.createdAt'>
+              <q-card-section>
+                <div class='font-bold'>
+                  Created {{formatDateAndTime(log.createdAt).formatted}}
+                  <br>
+                  {{formatDateAndTime(log.createdAt).formattedDistance}}
+                </div>
+                <div class='grid grid-cols-2' v-for='(displayName, name) in displayNameMappings' :key='name'>
+                  <p class='p-0 m-0' >
+                    {{displayName}}
+                  </p>
+                  <div v-if='!log[name]' class='text-green-500'>
+                    <q-icon name='fas fa-check-circle'/>
+                    No
+                  </div>
+                  <div v-else class='text-red-500'>
+                    <q-icon name='fas fa-exclamation-triangle'/>
+                    Yes
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <div v-else>
+          <empty-placeholder :loading='loadingLogs' title='No logs found for this user' icon='fas fa-times' subtitle="They just didn't submit anything yet."/>
+        </div>
       </div>
-    </div>
-    <div v-else>
-      <p class='text-gray-500'>No logs found for this user.</p>
+      <user-info-card v-if='activeUser' :user='activeUser'/>
+      <q-spinner v-else/>
     </div>
   </q-page>
 </template>
 
 <script lang='ts'>
+import UserInfoCard from 'components/UserInfoCard.vue';
+import EmptyPlaceholder from 'components/EmptyPlaceholder.vue';
 import { format, formatDistanceToNow } from 'date-fns';
 import store from 'src/api/store';
 import Vue from 'vue'
@@ -54,15 +63,16 @@ const displayNameMappings = {
 
 export default Vue.extend({
   name: "ViewLogs",
+  components: { EmptyPlaceholder, UserInfoCard },
   activated() {
-    if (store.userShownInLogs === null) {
-      this.$router.back();
-    }
+    this.activeUser = null;
     this.logs = [];
     this.getLogs();
   },
   data() {
     return {
+      activeUser: null,
+      loadingLogs: false,
       store,
       logs: [],
       displayNameMappings,
@@ -77,13 +87,23 @@ export default Vue.extend({
       }
     },
     async getLogs() {
+      this.loadingLogs = true
       try {
-        const response = await this.$axios.post('/admin/viewLogs', {userId: store.userShownInLogs.id});
-        if (response.data.result) {
-          this.logs = response.data.data;
+        const response1 = await this.$axios.get(`/admin/getUser/${this.$route.params.id}`);
+        if (response1.data.result) {
+          this.activeUser = Object.assign({}, this.activeUser, response1.data.data)
+          const response2 = await this.$axios.post('/admin/viewLogs', {userId: this.$route.params.id});
+          if (response2.data.result) {
+            this.logs = response2.data.data;
+          } else {
+            this.$q.notify({
+              message: `Getting logs failed: ${response2.data.message}`,
+              type: 'negative',
+            })
+          }
         } else {
           this.$q.notify({
-            message: `Getting logs failed: ${response.data.message}`,
+            message: `Getting logs failed: ${response1.data.message}`,
             type: 'negative',
           })
         }
@@ -93,6 +113,7 @@ export default Vue.extend({
           type: 'negative',
         })
       }
+      this.loadingLogs = false;
     }
   }
 })
