aba9e074892c10131b1963cfeb08db9a6b7959e6
fractaal <sonoroustwo@gmail.com>
2021-04-13 15:36:19 +0800

    ✨ config interface merge to config module 🔥 setConfig deprecated

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,45 +1,45 @@
-import Storage from 'node-persist';
-import Config from "./interfaces/config";
 import Logger from './logger';
+
+export interface Config {
+    location: string;
+    lguUrl: string;
+    secret: string;
+    httpPort: string;
+    httpsPort: string;
+    adminPassword: string;
+}
 
 const logger = Logger('Config')
 
 export async function getConfig(): Promise<Config> {
-    if (typeof Storage.get !== 'function') {
-        await Storage.init({dir: 'tracvac-config'})
-        logger.success('Configuration loaded!');
-    }
+    const config = {
+        location: process.env.LOCATION ,
+        lguUrl: process.env.LGUURL ,
+        secret: process.env.SECRET ,
+        httpsPort: process.env.PORT_SECURE ?? 443 ,
+        httpPort: process.env.PORT ?? 80 ,
+        adminPassword: process.env.ADMIN_PASSWORD ,
+    } as Partial<Config>;
 
-    const config = await Storage.get('config') as Config;
-
-    return {
-        location: process.env.LOCATION ?? config?.location,
-        lguUrl: process.env.LGUURL ?? config?.lguUrl,
-        secret: process.env.SECRET ?? config?.secret,
-        httpsPort: process.env.PORT_SECURE ?? config?.httpsPort ?? 443,
-        httpPort: process.env.PORT ?? config?.httpPort ?? 80,
-        adminPassword: process.env.ADMIN_PASSWORD,
-    } as Config;
-}
-
-export async function isProperlyConfigured(): Promise<boolean> {
-    const config = await getConfig() as Partial<Config>;
-
-    return typeof config.httpPort === 'string' &&
+    if (typeof config.httpPort === 'string' &&
         typeof config.httpsPort === 'string' &&
         typeof config.secret === 'string' &&
         typeof config.lguUrl === 'string' &&
         typeof config.location === 'string' &&
-        typeof config.adminPassword === 'string'
+        typeof config.adminPassword === 'string') {
+        return config as Config;
+    } else {
+        logger.error('Server is improperly configured! Check configuration and try again.')
+        process.exit(1);
+    }
 }
 
+/**
+ * @deprecated The browser should not be able to change the server configuration anymore
+ */
 export async function setConfig(config: Config): Promise<boolean> {
     try {
-        if (typeof Storage.get !== 'function') {
-            await getConfig();
-        }
-        await Storage.set('config', config);
-        return true;
+        return false;
     } catch (e) {
         logger.error('Failed to save configuration: ' + e);
         return false;
