11116ed9ab4976e9122d7f159c68552172776151
fractaal <sonoroustwo@gmail.com>
2021-04-13 22:02:26 +0800

    🔥 QInfiniteScroll -> ✨ LoadMore ✨ Use NotificationCard.vue

--- client/src/pages/Notifications.vue
+++ client/src/pages/Notifications.vue
@@ -2,67 +2,60 @@
   <q-page>
     <custom-header title="Notifications"/>
     <div class="px-8">
-      <q-infinite-scroll :debounce="300" ref="scroll" @load="onLoad" :offset="50">
+      <transition name="transition" mode="out-in">
+        <empty-placeholder
+          v-if="!isLoading && notifications.length === 0"
+          icon="fas fa-question"
+          title="No notifications found"
+          subtitle='LGU Notifications are shown here.'
+        />
+      </transition>
+      <load-more ref="list" @load="onLoad">
         <transition-group name="transition" mode="out-in">
-          <div
-          class="rounded-lg p-4 border border-solid border-gray-200 mb-4"
-          v-for="notification in notifications"
-          :key="notification.id"
-          >
-
-            <p class="text-h4 m-0">{{notification.title}}</p>
-            <p class="m-0" v-html="notification.content"></p>
-          </div>
-        </transition-group>
-        <template v-slot:loading>
-          <q-spinner-puff
-            class="block mx-auto"
-            color="primary"
-            size="4em"
+          <notification-card
+            v-for="notification in notifications"
+            :notification="notification"
+            :key="notification.id"
           />
-        </template>
-      </q-infinite-scroll>
+        </transition-group>
+      </load-more>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue, { VueConstructor } from 'vue'
 import { getNotifications, Notification } from 'src/api/notification'
 import { store } from 'src/api/store'
 import CustomHeader from 'components/CustomHeader.vue'
+import LoadMore from 'components/LoadMore.vue'
+import EmptyPlaceholder from 'components/EmptyPlaceholder.vue'
+import NotificationCard from 'components/NotificationCard.vue'
 
-export default Vue.extend({
+export default (Vue as VueConstructor<Vue & { $refs: {list: InstanceType<typeof LoadMore>}}>).extend({
   name: 'Notifications',
-  components: { CustomHeader },
-  activated () {
-    this.notifications = []
-    // @ts-ignore
-    this.$refs.scroll.reset()
-    // @ts-ignore
-    this.$refs.scroll.resume()
-    // @ts-ignore
-    this.$refs.scroll.trigger()
-  },
+  components: { NotificationCard, CustomHeader, LoadMore, EmptyPlaceholder, NotificationCard },
   data () {
     return {
+      isLoading: false,
       store,
-      notifications: [] as Notification[],
-      lastIndex: -1
+      notifications: [] as Notification[]
     }
+  },
+  activated () {
+    this.notifications = []
+    this.$refs.list.reset()
+    this.$refs.list.load()
   },
   methods: {
     async onLoad (index: number, done: Function) {
-      if (this.lastIndex === index) {
-        done()
-        return
-      }
-      this.lastIndex = index
-      const [notifs, isEnd] = await getNotifications(index - 1)
+      this.isLoading = true
+      const [notifs, isEnd] = await getNotifications(index)
       if (notifs !== null) {
         this.notifications.push(...notifs)
       }
       done(isEnd)
+      this.isLoading = false
     }
   }
 })
