42eb7cc871c3bf5ebc070258e6e4f23818576c93
fractaal <sonoroustwo@gmail.com>
2021-04-13 21:58:48 +0800

    ✨ EmptyPlaceholder component

---
+++ client/src/components/EmptyPlaceholder.vue
@@ -0,0 +1,31 @@
+<template>
+  <div  class="p-8 mt-32 text-gray-500 text-center" key="ph">
+    <q-icon class="block mx-auto" :name="icon" size="96px"/>
+    <p class="m-0 font-bold">{{title}}</p>
+    <p class="text-xs"><i>{{subtitle}}</i></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyPlaceholder',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
