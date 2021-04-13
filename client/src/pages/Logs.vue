dda90108ab52dcd3aecbc2427657781e3ce08ba7
fractaal <sonoroustwo@gmail.com>
2021-04-13 22:01:59 +0800

    🔥 QInfiniteScroll -> ✨ LoadMore

--- client/src/pages/Logs.vue
+++ client/src/pages/Logs.vue
@@ -3,66 +3,54 @@
     <custom-header title="Logs"/>
     <div class="px-8">
       <button-card title="Create A Log" @click="$router.push('/view-log')" icon="fas fa-pen-fancy"/>
-      <div v-if="!isLoading && store.logs.length === 0" class="p-8 mt-32 text-gray-500 text-center">
-        <q-icon class="block mx-auto" name="fas fa-question" size="96px"/>
-        <p class="m-0 font-bold">No logs found</p>
-        <p class="text-xs"><i>You can create one by pressing the "Create a Log" button.</i></p>
-      </div>
-      <!-- <div v-else-if="!isLoading && store.logs.length !== 0"> -->
+      <transition name="transition" mode="out-in">
+        <empty-placeholder
+          v-if="!isLoading && store.logs.length === 0"
+          icon="fas fa-question"
+          title="No logs found"
+          subtitle='You can create one by pressing the "Create a Log" button.'/>
+      </transition>
       <div>
-        <q-infinite-scroll :debounce="300" ref="scroll" @load="onLoad" :offset="50">
+        <load-more @load="onLoad" ref="list">
           <transition-group name="transition" mode="out-in">
             <log-card v-for="log in store.logs" :log="log" :key="log.createdAt"/>
           </transition-group>
-          <template v-slot:loading>
-            <q-spinner-puff
-              class="block mx-auto"
-              color="primary"
-              size="4em"
-            />
-          </template>
-        </q-infinite-scroll>
+        </load-more>
       </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue, { VueConstructor } from 'vue'
 import { getLogs, resetLogs } from 'src/api/logs'
 import { store } from 'src/api/store'
 import ButtonCard from 'components/ButtonCard.vue'
 import LogCard from 'components/LogCard.vue'
 import CustomHeader from 'components/CustomHeader.vue'
+import LoadMore from 'components/LoadMore.vue'
+import EmptyPlaceholder from 'components/EmptyPlaceholder.vue'
 
-export default Vue.extend({
+export default (Vue as VueConstructor<Vue & { $refs: {list: InstanceType<typeof LoadMore>}}>).extend({
   name: 'Logs',
-  components: { ButtonCard, LogCard, CustomHeader },
+  components: { EmptyPlaceholder, ButtonCard, LogCard, CustomHeader, LoadMore },
   data () {
     return {
       store,
-      isLoading: false,
-      lastIndex: -1
+      isLoading: false
     }
   },
   activated () {
-    console.log(this.$refs.scroll)
     resetLogs()
-    // @ts-ignore
-    this.$refs.scroll.reset()
-    // @ts-ignore
-    this.$refs.scroll.resume()
-    // @ts-ignore
-    this.$refs.scroll.trigger()
+    this.$refs.list.reset()
+    this.$refs.list.load()
   },
   methods: {
     async onLoad (index: number, done: Function) {
-      if (this.lastIndex === index) {
-        done()
-        return
-      }
-      const [success, isEnd] = await getLogs(index - 1)
+      this.isLoading = true
+      const [success, isEnd] = await getLogs(index)
       done(isEnd)
+      this.isLoading = false
     }
   }
 })
