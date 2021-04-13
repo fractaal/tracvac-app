57a0d61093d95306b1ed23856760b00a6cc398b8
fractaal <sonoroustwo@gmail.com>
2021-04-14 00:28:01 +0800

    ✨ icons

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -50,6 +50,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
+      distDir: '../hestia-backend/app',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
