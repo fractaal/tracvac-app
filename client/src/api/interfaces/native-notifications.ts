1c1393c9c4f9dedd4197c7eb4e4b86e99d71a832
Ben Jude <sonoroustwo@gmail.com>
2021-04-29 00:07:09 +0800

    nvm

--- client/src/api/interfaces/native-notifications.ts
+++
@@ -1,7 +0,0 @@
-export default interface NativeNotificationService {
-  readonly showNotifications: boolean;
-  readonly showVaccineNotifications: boolean;
-  readonly showLguNotifications: boolean;
-  enable(): boolean;
-  disable(): boolean;
-}
