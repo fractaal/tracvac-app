dafd09df2b07504eb5094fb1d48ef7520ecc2df8
Ben Jude <sonoroustwo@gmail.com>
2021-04-29 11:55:42 +0800

    fix userId incorrectly being used in query as subId

--- server/src/push.ts
+++ server/src/push.ts
@@ -4,11 +4,12 @@ import Logger from './logger'
 
 const logger = Logger('Push')
 export async function notifyUser(id: number, data: Record<string,any>) {
-  const subs = await PushSubscriptionModel.query().select('*').where({id})
+  logger.log(`Web push for ${id} commencing`)
+  const subs = await PushSubscriptionModel.query().select('*').where({userId: id})
   for (const sub of subs) {
     try {
       await webpush.sendNotification(sub.subscription as webpush.PushSubscription, JSON.stringify(data));
-      logger.log(`web push successful for ${sub.userId}`);
+      logger.log(`web push successful for ${id} at sub id ${sub.id}`);
     } catch(err) {
       logger.error(`web push failed! ${err}`);
     }
