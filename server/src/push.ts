2084e8c044f71c77106130c98f9546d3e8b063bb
Ben Jude <sonoroustwo@gmail.com>
2021-04-30 13:23:04 +0800

    refactor push function

--- server/src/push.ts
+++ server/src/push.ts
@@ -5,13 +5,18 @@ import Logger from './logger'
 const logger = Logger('Push')
 export async function notifyUser(id: number, data: Record<string,any>) {
   logger.log(`Web push for ${id} commencing`)
+  let success = true;
   const subs = await PushSubscriptionModel.query().select('*').where({userId: id})
+  
   for (const sub of subs) {
     try {
       await webpush.sendNotification(sub.subscription as webpush.PushSubscription, JSON.stringify(data));
-      logger.log(`web push successful for ${id} at sub id ${sub.id}`);
+      logger.log(`Web push successful for ${id} at subscription id ${sub.id}`);
     } catch(err) {
-      logger.error(`web push failed! ${err}`);
+      logger.warn(`Web push failed - ${err}`);
+      success = false;
     }
   }
+
+  return success;
 }
