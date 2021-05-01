8cc4866ebfe32fe3adb4d72332f496fd2ae721ae
Ben Jude <sonoroustwo@gmail.com>
2021-05-02 02:05:06 +0800

    change output dir

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -50,7 +50,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      distDir: '../hestia-backend/app',
+      distDir: '../server/app',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
