dc0f5493b705c69e806382a9502a69dfb292c3f1
Ben Jude <sonoroustwo@gmail.com>
2021-05-08 15:15:59 +0800

    :construction: add insight component

---
+++ admin/src/pages/Insight.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="p-8">
+    <p class="-my-2">TRACVAC</p>
+    <h4 class="m-0 mb-4 font-light">INSIGHT</h4>
+    <div class="grid grid-cols-3 grid-rows-3 gap-4 grid-flow-col-dense">
+      <div v-for="index in 20" class="p-2 rounded-md shadow-xl border-2 border-solid border-gray-200">
+        qofuwuhfo
+      </div>
+    </div>
+    <q-inner-loading :showing="false" size="128px"/>
+  </q-page>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Insight',
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
