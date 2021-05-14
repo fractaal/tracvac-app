2b9a19773af78ffd9f4a043ebc04cb74cd2fb506
Ben Jude <sonoroustwo@gmail.com>
2021-05-14 21:00:39 +0800

    :bug: Fix longstanding bug
    Tracvac Administrator would not get server config on initial page load, meaning the UI elements that showed the server name and location were left blank until you opened the configuration menu. This is fixed now.

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -5,10 +5,12 @@
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import { getConfig } from './api/config';
 
 export default Vue.extend({
   name: 'App',
   created() {
+    void getConfig()
     this.$q.loadingBar.setDefaults({size: '10px'})
   }
 });
