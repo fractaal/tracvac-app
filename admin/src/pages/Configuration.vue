9ad1f5bfea474665de8adc57aa1bd0ed73be7818
Ben Jude <sonoroustwo@gmail.com>
2021-05-14 20:48:06 +0800

    :construction: Generalize terms

--- admin/src/pages/Configuration.vue
+++ admin/src/pages/Configuration.vue
@@ -2,7 +2,9 @@
   <q-page class="p-8">
     <h4 class="m-0 font-light">CONFIGURATION</h4>
     <div class="my-8 mx-auto w-1/2">
-      <q-input class="mt-8" label="Municipality" v-model="store.serverConfig.location" disable/>
+      <q-input class="mt-8" label="Name" v-model="store.serverConfig.name" disable/>
+      <p class="my-1">The name of this Tracvac site. It could be the company name, or the name of the LGU, so long as it's a good indicator of who's hosting this site.</p>
+      <q-input class="mt-8" label="Location" v-model="store.serverConfig.location" disable/>
       <p class="my-1">Where is this Tracvac site physically situated in?</p>
       <q-input class="mt-8" label="Local Government Unit Website" v-model="store.serverConfig.lguUrl" disable/>
       <p class="my-1">Website address of this LGU.<br><i>Note: If you leave this blank, the button that leads to the LGU website in the app will not be shown.</i></p>
