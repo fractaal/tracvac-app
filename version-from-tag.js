b87431e9d22d5ea1405bd5f7bba975c4649e640c
Ben Jude <sonoroustwo@gmail.com>
2021-05-19 02:16:25 +0800

    :green_heart: Conditionally commit depending on version is already the same or not

--- version-from-tag.js
+++ version-from-tag.js
@@ -3,9 +3,14 @@ const { execSync } = require('child_process')
 const package = require('./package.json')
 
 const getLatestTag = () => {
-  return execSync('git describe --tags $(git rev-list --tags --max-count=1)', {encoding: "utf-8"}).trim()
+  return execSync('git describe --tags $(git rev-list --tags --max-count=1)', {encoding: "utf-8"}).trim().substring(1, 999)
 }
 
-package.version = getLatestTag()
-
-writeFileSync("./package.json", JSON.stringify(package, null, 2))
+if (package.version == getLatestTag()) {
+  console.log("Not changing version - already the same.")
+} else {
+  package.version = getLatestTag()
+  writeFileSync("./package.json", JSON.stringify(package, null, 2))
+  console.log(execSync('git add package.json', {encoding: 'utf-8'}))
+  console.log(execSync('git commit -m "bump version"', {encoding: 'utf-8'}))
+}
