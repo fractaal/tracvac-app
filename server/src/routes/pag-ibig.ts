c825080b780114ac355510e518b26e157c8e4329
Ben Jude <sonoroustwo@gmail.com>
2021-06-07 18:26:40 +0800

    :construction: Pag-Ibig Fund specific functionality module

---
+++ server/src/routes/pag-ibig.ts
@@ -0,0 +1,31 @@
+/**
+ * Pag-Ibig specific module
+ */
+
+import { knex } from "../database";
+import { app } from "../index";
+import { UserModel as _ } from "../database/models/UserModel";
+
+// Pag-Ibig related data
+class UserModel extends _ {
+    isDeferredFirst!: boolean;
+    isDeferredSecond!: boolean;
+    healthDeclarationData!: Record<string,any>;
+}
+
+(async () => {
+    // Perform schema changes to the user table
+    await knex.schema.table('users', table => {
+        table.boolean('isDeferredFirst')
+        table.boolean('isDeferredSecond')
+        table.json('healthDeclarationData')
+    })
+
+    app.post('/healthDeclarationForm', async (req, res) => {
+        // TODO
+    })
+
+})();
+
+
+
