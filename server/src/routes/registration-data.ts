0282afb362aac6a175388798270ce5a6e589f482
Ben Jude <sonoroustwo@gmail.com>
2021-06-15 11:23:14 +0800

    :wastebasket: deprecate RegistrationData in favor of UserRegistrationFields

--- server/src/routes/registration-data.ts
+++ server/src/routes/registration-data.ts
@@ -1,23 +1,27 @@
-import { app } from '../'
-import { getConfig } from '../config';
+/**
+ * @deprecated This module is removed in favor of the new modular plugins system. 
+ */
 
-app.get('/registrationData', async (req, res) => {
-  if ((await getConfig()).isCorporation) {
-    res.json([
-      {
-        title: "Company Information",
-        description: "Information relating to your workplace.",
-        formItems: [
-          {
-            name: "companyBranch",
-            displayName: "Company Branch",
-            type: "string",
-            format: "Text"
-          }
-        ]
-      }
-    ])
-  } else {
-    res.json([])
-  }
-})
+// import { app } from '../'
+// import { getConfig } from '../config';
+
+// app.get('/registrationData', async (req, res) => {
+//   if ((await getConfig()).isCorporation) {
+//     res.json([
+//       {
+//         title: "Company Information",
+//         description: "Information relating to your workplace.",
+//         formItems: [
+//           {
+//             name: "companyBranch",
+//             displayName: "Company Branch",
+//             type: "string",
+//             format: "Text"
+//           }
+//         ]
+//       }
+//     ])
+//   } else {
+//     res.json([])
+//   }
+// })
