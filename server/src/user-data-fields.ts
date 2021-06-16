28992f095dcdcc4b73ee4e4b8355557fc17feae2
Ben Jude <sonoroustwo@gmail.com>
2021-06-17 02:20:41 +0800

    :sparkles: make UserDataFields actually functional

--- server/src/user-data-fields.ts
+++ server/src/user-data-fields.ts
@@ -1,4 +1,5 @@
 import Logger from "./logger"
+import { knex } from "./database"
 
 const logger = Logger("UserDataFields");
 interface UserDataField {
@@ -11,7 +12,20 @@ interface UserDataField {
 
 const dataFields: UserDataField[] = []
 
-export const addDataField = async (data: UserDataField | UserDataField[]) => {
+export const addDataField = async (data: UserDataField[]) => {
 	if (!Array.isArray(data)) data = [data]
+
+	await knex.schema.table("users", async (table) => {
+		await Promise.all(data.map(async (item) => {
+			if (!await knex.schema.hasColumn("users", item.name)) {
+				logger.log(`Creating new column in user table ${item.name}`)
+				// @ts-ignore
+				table[item.type](item.name)
+			} else {
+				logger.log(`Not creating column ${item.name} in user table - already exists.`)
+			}
+		}))
+	})
+
 	dataFields.push(...data)
 }
