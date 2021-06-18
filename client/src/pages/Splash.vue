70a7beb57b672de0c528ee8520feb3c00190a990
Ben Jude <sonoroustwo@gmail.com>
2021-06-18 22:23:39 +0800

    :lipstick: "passport" -> "tracker"

--- client/src/pages/Splash.vue
+++ client/src/pages/Splash.vue
@@ -2,7 +2,7 @@
   <q-page class="column content-center">
     <img src="~assets/tracvac-logo.png" class="w-64 mt-44 bobbing bobbing-fast mx-auto" alt="" />
     <div class="text-4xl text-center logo-text">Tracvac</div>
-    <p class="m-0 p-0 text-center">Your friendly digital vaccine passport</p>
+    <p class="m-0 p-0 text-center">Your friendly digital vaccine tracker</p>
   </q-page>
 </template>
 
