9ee51effb580db8dc7dd6e6653260b91f0ddfda8
Ben Jude <sonoroustwo@gmail.com>
2021-06-20 14:41:11 +0800

    :bug: Fix bug where UserDataFields would not create columns

--- server/src/user-data-fields.ts
+++ server/src/user-data-fields.ts
@@ -17,16 +17,22 @@ export const getDataFields = () => dataFields
 export const addDataFields = async (data: UserDataField[]) => {
 	if (!Array.isArray(data)) data = [data]
 
+	const fields: Record<string, string> = {}
+	await Promise.all(data.map(async (item) => {
+		if (!await knex.schema.hasColumn("users", item.name)) {
+			// @ts-ignore
+			fields[item.type] = item.name
+		} else {
+			logger.log(`Not creating column ${item.name} in user table - already exists.`)
+		}
+	}))
+	
 	await knex.schema.table("users", async (table) => {
-		await Promise.all(data.map(async (item) => {
-			if (!await knex.schema.hasColumn("users", item.name)) {
-				logger.log(`Creating new column in user table ${item.name}`)
-				// @ts-ignore
-				table[item.type](item.name)
-			} else {
-				logger.log(`Not creating column ${item.name} in user table - already exists.`)
-			}
-		}))
+		for (const [type, name] of Object.entries(fields)) {
+			logger.log(`Creating new column ${name} of type ${type}`)
+			// @ts-ignore
+			table[type](name)
+		}
 	})
 
 	dataFields.push(...data)
