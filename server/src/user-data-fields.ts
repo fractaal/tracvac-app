0d41241a4f14034619e673733475cb438061325d
Ben Jude <sonoroustwo@gmail.com>
2021-06-24 22:27:17 +0800

    :bug: fix bug where other workers would error out in creating field
    Only let the first worker create the data fields

--- server/src/user-data-fields.ts
+++ server/src/user-data-fields.ts
@@ -1,4 +1,5 @@
 import Logger from "./logger"
+import cluster from "cluster"
 import { knex } from "./database"
 
 const logger = Logger("UserDataFields")
@@ -27,13 +28,14 @@ export const addDataFields = async (data: UserDataField[]) => {
     data.map(async (item) => {
       
       // Supported field checking
-      if (!supportedDataFieldTypes.includes(item.type)) {
+      if (!supportedDataFieldTypes.includes(item.type) && item.isShownInAdmin) {
         logger.warn(`User data field ${item.displayName} has an unsupported type ${item.type}. It will still appear in the administrator interface, but your mileage may vary.`)
       }
 
       if (!(await knex.schema.hasColumn("users", item.name))) {
         // @ts-ignore
-        fields[item.type] = item.name
+        if (cluster.isWorker && cluster.worker.id === 1) fields[item.type] = item.name
+        // Only actually create these fields if you're the first worker
       } else {
         logger.log(
           `Not creating column ${item.name} in user table - already exists.`
