a88d3d816a988fcaec5c3833afa8f99cb4777703
Ben Jude <sonoroustwo@gmail.com>
2021-06-28 01:04:58 +0800

    :wrench: Turn off rules

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -80,6 +80,7 @@ module.exports = {
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
     '@typescript-eslint/restrict-template-expressions': 'warn',
     '@typescript-eslint/no-floating-promises': 'warn',
     '@typescript-eslint/no-unsafe-assignment': 'warn',
